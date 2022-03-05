19aa9dfc955ebcf35815c0d528b0438e56773bc5
Rachel Gray <rachel.gray7448@gmail.com>
2022-03-04 23:35:57 -0500

    basic html outline and question variable

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+var myQuestions = [
+    {
+        question: "Commonly used data types do NOT include:",
+        answers: {
+            a: "Strings",
+            b: "Booleans",
+            c: "Alerts",
+            d: "Numbers"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with:",
+        answers: {
+            a: "quotes",
+            b: "curly brackets",
+            c: "parenthesis",
+            d: "square brackets"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Arrays in Javascript can be used to store:",
+        answers: {
+            a: "Numbers and strings",
+            b: "Other arrays",
+            c: "Booleans",
+            d: "All of the above"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        answers: {
+            a: "commas",
+            b: "curly brackets",
+            c: "quotes",
+            d: "parenthesis"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "A very useful tool used during developement and debugging for printing content to the debugger is:",
+        answers: {
+            a: "Javascript",
+            b: "Terminal/bash",
+            c: "For loops",
+            d: "Console.log"
+        },
+        correctAnswer: "d"
+    }
+];
+
+var score = 0;
+
+for (i=0, i < myQuestions.length, i++) {
+
+}
